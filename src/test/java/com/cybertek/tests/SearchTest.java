2a7e997334dc4c5abb7f349488a9321e4c7d2050
TenBarrel6 <ruslan21140@gmail.com>
2019-06-29 16:16:03 -0400

    pom update

---
+++ src/test/java/com/cybertek/tests/SearchTest.java
@@ -0,0 +1,58 @@
+package com.cybertek.tests;
+
+import com.cybertek.utilities.StringUtility;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.awt.image.Kernel;
+
+public class SearchTest {
+    public static void main(String[] args) throws InterruptedException{
+        ebaySearch();
+        amazonSearch();
+        wikiSearch();
+    }
+
+    public static void ebaySearch()throws InterruptedException{
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.ebay.com/");
+        driver.findElement(By.id("gh-ac")).sendKeys("wooden spoon");
+        Thread.sleep(1500);
+        driver.findElement(By.id("gh-btn")).click();
+        Thread.sleep(1500);
+        String actualResults = driver.findElement(By.className("srp-controls__count-heading")).getText();
+        System.out.println(actualResults);
+        driver.quit();
+    }
+
+    public static void amazonSearch()throws InterruptedException{
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.amazon.com/");
+        driver.findElement(By.id("twotabsearchtextbox")).sendKeys("wooden spoon");
+        Thread.sleep(1500);
+        driver.findElement(By.className("nav-input")).click();
+        Thread.sleep(1500);
+        String actualResults = driver.findElement(By.className("sg-col-inner")).getText();
+        System.out.println(actualResults);
+        driver.quit();
+    }
+
+    public static void wikiSearch()throws InterruptedException{
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.wikipedia.org/");
+        driver.findElement(By.id("searchInput")).sendKeys("selenium webdriver");
+        Thread.sleep(1500);
+        driver.findElement(By.tagName("button")).click();
+        Thread.sleep(1500);
+        driver.findElement(By.className("searchmatch")).click();
+        Thread.sleep(1500);
+        System.out.println(driver.getCurrentUrl().endsWith("Selenium_(software)"));
+        driver.quit();
+    }
+}
