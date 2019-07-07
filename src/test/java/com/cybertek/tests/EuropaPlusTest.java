25384dd16086413f89b4402de878d6f62b5cae56
TenBarrel6 <ruslan21140@gmail.com>
2019-07-07 18:51:02 -0400

    SeleniumTestNG

--- src/test/java/com/cybertek/tests/EuropaPlusTest.java
+++ src/test/java/com/cybertek/tests/EuropaPlusTest.java
@@ -7,10 +7,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.Select;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class EuropaPlusTest {
     public static void main(String[] args) throws InterruptedException{
